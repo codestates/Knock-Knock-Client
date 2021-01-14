effd723935be17bba19b9ac2b5d9c591cc8ae839
Tate <tate@Tateui-MacBookPro.local>
2021-01-15 00:53:29 +0900

    done with layout

--- knock-client-app/src/App.js
+++ knock-client-app/src/App.js
@@ -1,37 +1,68 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React, { useEffect, useState } from "react";
-const axios = require("axios");
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "../src/styles/nav.css";
+import PublicBoard from "../src/components/board/board";
+import Home from "../src/components/main/Home";
 
 const App = () => {
-  const [userName, setUserName] = useState("");
-
-  const registerName = (name) => {
-    setUserName(name.target.value);
-    console.log("히어", name.target.value);
-  };
-
-  const checkName = () => {
-    axios("/register", {
-      method: "get",
-      body: JSON.stringify(userName),
-    });
-  };
-
   return (
-    <div className="App">
-      <div>
-        <p>Register yourname</p>
-        <textarea placeholder="fill up with your name"></textarea>
-        <button onClick={registerName}>Register</button>
-      </div>
-      <div>
-        <p>Check yourname registered</p>
-        <textarea placeholder="fill up with your name"></textarea>
-        <button>check</button>
-      </div>
-    </div>
+    <Router>
+      <ul className="navbar">
+        <li>
+          <Link to="/">HOME</Link>
+        </li>
+        <li>
+          <Link to="/board">BOARD</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/board">
+          <PublicBoard />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
 export default App;
+
+// import logo from "./logo.svg";
+// import "./App.css";
+// import React, { useEffect, useState } from "react";
+// const axios = require("axios");
+
+// const App = () => {
+//   const [userName, setUserName] = useState("");
+
+//   const registerName = (name) => {
+//     setUserName(name.target.value);
+//     console.log("히어", name.target.value);
+//   };
+
+//   const checkName = () => {
+//     axios("/register", {
+//       method: "get",
+//       body: JSON.stringify(userName),
+//     });
+//   };
+
+//   return (
+//     <div className="App">
+//       <div>
+//         <p>Register yourname</p>
+//         <textarea placeholder="fill up with your name"></textarea>
+//         <button onClick={registerName}>Register</button>
+//       </div>
+//       <div>
+//         <p>Check yourname registered</p>
+//         <textarea placeholder="fill up with your name"></textarea>
+//         <button>check</button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
--- knock-client-app/src/components/main/Home.js
+++ knock-client-app/src/components/main/Home.js
@@ -1,19 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "../../styles/home.css";
-import Nav from "../nav";
+
 import explainImg from "../../images/explainImg.png";
 import testImg1 from "../../images/testImg1.png";
 
 const Home = () => {
   return (
-    <div className="container">
-      <header className="header">
-        <Nav />
-      </header>
-      <section className="sec1">
-        <div className="sec1_wrap1">
-          <div className="mainDoor">
-            <div className="doorLeft">
+    <div className="H_container">
+      <header className="H_header"></header>
+      <section className="H_sec1">
+        <div className="H_sec1_wrap1">
+          <div className="H_mainDoor">
+            <div className="H_doorLeft">
               <svg
                 width="209"
                 height="670"
@@ -24,7 +22,7 @@ const Home = () => {
                 <path d="M208.5 130V760L0 897.5V0L208.5 130Z" fill="#F7E462" />
               </svg>
             </div>
-            <div className="doorRight">
+            <div className="H_doorRight">
               <svg
                 width="209"
                 height="670"
@@ -40,33 +38,33 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="sec1_wrap2">
-          <div className="mouse">
-            <span className="mouseDot"></span>
+        <div className="H_sec1_wrap2">
+          <div className="H_mouse">
+            <span className="H_mouseDot"></span>
           </div>
         </div>
-        <div className="sec1_wrap3">
-          <div className="searchContainer">
-            <select className="boardType">
+        <div className="H_sec1_wrap3">
+          <div className="H_searchContainer">
+            <select className="H_boardType">
               <option value="">게시물 유형</option>
               <option value="project">프로젝트</option>
               <option value="study">스터디</option>
             </select>
-            <div className="searchBoxBoundary">|</div>
-            <select className="boardPeopleNum">
+            <div className="H_searchBoxBoundary">|</div>
+            <select className="H_boardPeopleNum">
               <option value="">인원수</option>
               <option value="2">2명</option>
               <option value="3">3명</option>
               <option value="4">4명</option>
               <option value="more">4명 이상</option>
             </select>
-            <div className="searchBoxBoundary">|</div>
-            <input type="text" className="boardSearchTextBox" />
-            <button className="boardSearchBtn">
-              <i class="fa fa-search"></i>
+            <div className="H_searchBoxBoundary">|</div>
+            <input type="text" className="H_boardSearchTextBox" />
+            <button className="H_boardSearchBtn">
+              <i className="H_fa fa-search"></i>
             </button>
           </div>
-          <div className="mainSimpleExplain">
+          <div className="H_mainSimpleExplain">
             <p>
               코딩 프로젝트, 스터디를 찾아주는 사이트입니다. 여러분이 원하는
             </p>
@@ -74,11 +72,11 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="sec2">
-        <div className="sec2_wrap1">
-          <img src={explainImg} className="explainImg" alt="" />
+      <section className="H_sec2">
+        <div className="H_sec2_wrap1">
+          <img src={explainImg} className="H_explainImg" alt="" />
         </div>
-        <div className="sec2_wrap2">
+        <div className="H_sec2_wrap2">
           <p>새로운 사람들과 함께 나아가고 여러분의 발자취를 남겨보세요!</p>
           <p>
             여러분의 스터디, 프로젝트를 차분히 시작하거나 직접 참여해보세요!
@@ -86,16 +84,16 @@ const Home = () => {
           <p>이제, 여러분의 시작을 두드리세요!</p>
         </div>
       </section>
-      <section className="sec3">
-        <div className="sec3_wrap1">
+      <section className="H_sec3">
+        <div className="H_sec3_wrap1">
           <p>2020년, 현재까지 447 개의 프로젝트를 완료했습니다</p>
         </div>
-        <div className="sec3_wrap2">
+        <div className="H_sec3_wrap2">
           <img src={testImg1} alt="" />
         </div>
       </section>
-      <section className="sec4"></section>
-      <footer className="homeFooter"></footer>
+      <section className="H_sec4"></section>
+      <footer className="H_homeFooter"></footer>
     </div>
   );
 };
