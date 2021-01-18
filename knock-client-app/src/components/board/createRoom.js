cb0abfedf01f58221d7b07fe6a64c1546d014403
jeonginsoo <macbookair13@jeong-insoo-Macbook-Air.local>
2021-01-18 18:21:24 +0900

    [MODIFY] edit mypage

--- knock-client-app/src/components/board/createRoom.js
+++ knock-client-app/src/components/board/createRoom.js
@@ -3,23 +3,53 @@ import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import DataForm from "./dataForm";
 import "../../styles/createRoom.css";
+const axios = require("axios");
 // import Context from "../../utils/context";
 
 const CreateRoom = () => {
   // const [category, setCategory] = useContext(Context).categoryContext;
   const [category, setCategory] = useState("");
-  const setting = (e) => {
-    setCategory(e.target.value);
+  const [title, setTitle] = useState("");
+  const [crew,setCrew]= useState("");
+  const [positon, setPosition] = useState("");
+  const [stack, setStack] = useState("");
+  const [description, setDescription] = useState("");
+  const [roominfo, setRoomInfo] = useState("");
+
+  const createRoom = () => {
+    axios({
+      url: "/createRoom",
+      method: "POST",
+      data: {},
+      withCredentials: true,
+      headers: "application/json",
+    });
   };
 
-  // const dataForm = () => {
+  const getTitle = (e) => {
+    !e.target.value ? setTitle("") : setTitle(e.target.value);
+  };
 
-  // }
+  const getCrew = (e)=>{
+    !e.target.value ? setCrew("") : setCrew(e.target.value)
+  }
+
+  const getPosition = (e) => {
+    !e.target.value ? setPosition("") : setPosition(e.target.value);
+  };
+
+  const getStack = (e) => {
+    !e.target.value ? setStack("") : setStack(e.target.value);
+  };
+
+  const getDescription = (e) => {
+    !e.target.value ? setDescription("") : setDescription(e.target.value);
+  };
 
   return (
     <div className="App">
       <h2>Create Post</h2>
-      <select onChange={(e) => setting(e)}>
+      <select onChange={(e) => setCategory(e.target.value)}>
         <option>카테코리를 선택해주세요</option>
         <option>Project</option>
         <option>Study</option>
@@ -28,7 +58,15 @@ const CreateRoom = () => {
       <div>
         <textarea placeholder="제목을 입력해주세요"></textarea>
       </div>
-      <DataForm category={category} />
+      <DataForm
+        category={category}
+        crew={getCrew}
+        title={getTitle}
+        position={getPosition}
+        stack={getStack}
+        description={getDescription}
+      />
+      
       <CKEditor
         editor={ClassicEditor}
         data="<h1>인수님 피곤해요?</h1>"
--- knock-client-app/src/components/main/MngHistory.js
+++ knock-client-app/src/components/main/MngHistory.js
@@ -3,21 +3,22 @@ import ProfileEdit from "./ProfileEditForm";
 import "../../styles/history.css";
 import "../../styles/mypage.css";
 import SendRetrospect from "./retrospect";
+const axios = require("axios");
+
 class MngHistory extends Component {
   constructor(props) {
     super(props);
 
-    this.registerJouranl = this.registerJouranl.bind(this);
-    this.keepJournal = this.keepJournal.bind(this);
-    this.IsOpen = this.IsOpen.bind(this);
-
     this.state = {
       isClick: false,
-      isOpen: false,
       journals: [],
     };
     this.journals = [];
     this.retrospect = "";
+
+    this.registerJouranl = this.registerJouranl.bind(this);
+    this.keepJournal = this.keepJournal.bind(this);
+    this.dangerBtn = this.dangerBtn.bind(this);
   }
 
   keepJournal(value) {
@@ -39,63 +40,48 @@ class MngHistory extends Component {
     return this.setState({ journals: this.journals });
   }
 
-  IsOpen() {
-    this.state.isOpen === false
-      ? this.setState({ isOpen: true })
-      : this.setState({ isOpen: false });
+  dangerBtn() {
+    axios({
+      method: "POST",
+      url: "/deletePost",
+      WithCredentials: true,
+      headers: { "content-Type": "application/json" },
+      body: JSON.stringify({ roomId: "몰라요", userId: "이것도몰라요" }),
+    });
   }
 
   render() {
-    // console.log("props", this.state);
     return (
       <div className="mypageContainer">
         <div className="mypageContainer_blankSec"></div>
         <ProfileEdit />
 
         <div className="mypageContainer_editUserInfoFormSec">
-          {this.state.isOpen === false ? (
-            <>
-              <div className="His_submitForm">
-                <textarea
-                  className="Journal_box"
-                  placeholder="Why not to keep a journal about what you did!!!??"
-                  onChange={(e) => this.keepJournal(e)}
-                />
+          <p></p>
+          <button
+            onClick={() => {
+              this.dangerBtn();
+            }}
+            className="DangerBtn"
+          >
+            레포삭제
+          </button>
+          <div className="His_submitForm">
+            <textarea
+              className="Journal_box"
+              placeholder="Why not to keep a journal about what you did!!!??"
+              onChange={(e) => this.keepJournal(e)}
+            />
 
-                <div className="His_submit">
-                  <button onClick={() => this.registerJouranl()}>
-                    <p className="His_submit_p">등록</p>
-                  </button>
-                </div>
-              </div>
-              <ul className="Retro_list">
-                <SendRetrospect value={this.state.journals} />
-              </ul>
-            </>
-          ) : (
-            <div className="His_closeJournalForm">
-              <div className="His_JournalForm">
-                <p className="Journal_username">IM24 정인수</p>
-                <p className="Journal_date">2021-01-03</p>
-                <p className="Journal_text">나는 오늘 css에게 버림받았다.</p>
-              </div>
-              <div className="His_JournalForm">
-                <p className="Journal_username">IM24 정인수</p>
-                <p className="Journal_date">2021-01-03</p>
-                <p className="Journal_text">나는 오늘 css에게 버림받았다.</p>
-              </div>
-              <div className="His_JournalForm">
-                <p className="Journal_username">IM24 정인수</p>
-                <p className="Journal_date">2021-01-03</p>
-                <p className="Journal_text">나는 오늘 css에게 버림받았다.</p>
-              </div>
-              <div className="His_JournalForm">
-                <p className="Journal_username">IM24 정인수</p>
-                <p className="Journal_date">2021-01-03</p>
-                <p className="Journal_text">나는 오늘 css에게 버림받았다.</p>
-              </div>
+            <div className="His_submit">
+              <button onClick={() => this.registerJouranl()}>
+                <p className="His_submit_p">등록</p>
+              </button>
             </div>
-          )}
+          </div>
+          <ul className="Retro_list">
+            <SendRetrospect value={this.state.journals} />
+          </ul>
         </div>
       </div>
     );
